4c6bf43ec31cd1a8d3c22c07fbfc2a7d95b77190
钱紫昌 <487975196@qq.com>
2025-12-31 13:21:08 +0800

    githup，gitee地址

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -7,8 +7,8 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '主页', link: '/' },
+      { text: '示例', link: '/markdown-examples' }
     ],
 
     sidebar: [
@@ -22,7 +22,8 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/dovfer43979' },
+      { icon: 'gitee', link: 'https://gitee.com/dovfer' }
     ],
     footer:{
       copyright: 'Copyright © 2025'
