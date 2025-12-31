27449e67250b7a4ae35d131e41d180ff8252eca5
钱紫昌 <487975196@qq.com>
2025-12-31 09:34:12 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "多弗的VitePress文档",
+  description: "一个VitePress文档",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer:{
+      copyright: 'Copyright © 2025'
+    }
+  },
+  base: '/doc-demo',
+})
---
+++ index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "多弗的VitePress文档"
+  text: "一个VitePress文档"
+  tagline: 记录
+  actions:
+    - theme: brand
+      text: 第一个示例
+      link: /markdown-examples
+    - theme: alt
+      text: 第二个示例
+      link: /api-examples
+
+features:
+  - title: 特性1
+    details: 特性描述
+  - title: 特性2
+    details: 特性2描述
+  - title: 特性3
+    details: 特性3描述
+---
+
